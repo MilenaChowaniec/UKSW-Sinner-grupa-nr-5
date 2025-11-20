215036fb789a88d6049a918b48bfd8950e2a77b4
agnieszkaengel <agnieszka.engel05@gmail.com>
2025-11-20 12:09:25 +0100

    dodano komentarze zgodne z dokumentacją do skryptów

--- scripts/krata.gd
+++ scripts/krata.gd
@@ -1,9 +1,16 @@
 extends Area2D
-var Player_Is_In:bool
+##Klasa Krata obsługuje obiekty krat w rozgrywce.
+class_name Krata
 @export var krata_animation: AnimatedSprite2D
 
+#to docelowo albo żeby dopiero jak zabijemy wroga się otwierało:
+## Funkcja wywoływana gdy gracz wejdzie w obszar kolizji obiektu kraty.
+## Funkcja uruchamia animację otwierania kraty.
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		krata_animation.play("open_krata")
+	if body.name == "Player":
+		krata_animation.play("open")
 		
-	
+
+#to do testow bez gracza:
+func _on_mouse_entered() -> void:
+	krata_animation.play("open")
