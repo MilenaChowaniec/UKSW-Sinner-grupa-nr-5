f1a4dbd83fabac5eca78285815beaf119734a4a1
Milena <cho.milena123@gmail.com>
2025-11-23 17:01:48 +0100

    zmiany w architekurze plikow i dodano timer odliczajacy czas rozgrywki

--- scripts/bullet/bullet.gd
+++ scripts/bullet/bullet.gd
@@ -1,6 +1,6 @@
 class_name bullet extends Area2D
 
-@export var speed: float = 400.0 ## Bullet movement speed
+@export var speed: float = 700.0 ## Bullet movement speed
 @export var max_distance: float = 400 ## Max distance bullet can travel before exploding
 
 ## References to bullet sprites and for explosion animation
