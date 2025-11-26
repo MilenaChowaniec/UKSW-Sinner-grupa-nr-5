80df824bb28581a7815b03f9de93c6fb0dddb9c0
Milena <cho.milena123@gmail.com>
2025-11-26 19:48:37 +0100

    poprawki z wielkoscia pocisku oraz z hud

--- scripts/bullet/bullet.gd
+++ scripts/bullet/bullet.gd
@@ -1,6 +1,6 @@
 class_name bullet extends Area2D
 
-@export var speed: float = 200.0 ## Bullet movement speed
+@export var speed: float = 300.0 ## Bullet movement speed
 @export var max_distance: float = 100 ## Max distance bullet can travel before exploding
 
 ## References to bullet sprites and for explosion animation
