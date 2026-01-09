30c402aa363aa837b7c214103e6acc5e05d5a840
Wiktor Czarnota <weqtor66@gmail.com>
2026-01-09 02:37:40 +0100

    zainicjowanie podstawowej animacji idle dla ranger

--- scripts/enemy/Renger/renger.gd
+++ scripts/enemy/Renger/renger.gd
@@ -1,33 +1,55 @@
 class_name Renger extends CharacterBody2D
 
-@export var move_speed: float = 50.0
-@export var health: int = 100
-
+@export var move_speed: float = 150.0
+@export var health: int = 4
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var player_detection_zone = $PlayerDetectionZone
 
-var player: Node2D = null
-var current_state: String = "static_idle"
-var is_dead: bool = false
-var time_since_last_attack: float = 0.0
-var can_attack: bool = true
+var direction: Vector2 = Vector2.ZERO
+var state : String = "idle"
+var cardinal_direction : Vector2 = Vector2.LEFT
 
 func _ready():
 	pass
 
 
 func _process(delta):
-	var direction: Vector2 = Vector2.ZERO
+	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	velocity = direction * move_speed
+	
+	if SetState() == true:
+		UpdateAnimation()
 	
 	pass
 
 
 func _physics_process(delta):
 	move_and_slide()
-	if is_dead:
-		return
+	return
 	
+
+func SetDirection() -> bool:
+	return true
+
+func SetState() -> bool:
+	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	state = new_state
+	return true
+
+
+func UpdateAnimation() -> void:
+	animated_sprite.play("static_idle")
+	pass
+	
+	
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.LEFT:
+		return "left"
+	else:
+		return "right"
+		
