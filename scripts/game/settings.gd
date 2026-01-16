38cf2a264ea4023ecff4b07e9d4aecfbc8e6b812
agnieszkaengel <agnieszka.engel05@gmail.com>
2026-01-16 10:18:27 +0100

    dodano zapis ustawienia poziomu glosnosci

--- scripts/game/settings.gd
+++ scripts/game/settings.gd
@@ -1,5 +1,16 @@
 extends Node2D
-	
+
+func _ready():
+	var config = ConfigFile.new()
+	var err = config.load("user://settings.cfg")
+
+	if err == OK:
+		var value = config.get_value("audio", "master_volume", 100.0)
+		$HSlider.value = value
+
+		var db = linear_to_db(value / 100.0)
+		AudioServer.set_bus_volume_db(0, db)
+
 func _on_fullscreen_pressed() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
@@ -14,7 +25,11 @@ func _on_back_pressed() -> void:
 
 func _on_h_slider_value_changed(value: float) -> void:
 	var db = linear_to_db(value / 100.0)
-	AudioServer.set_bus_volume_db(0, db) # 0 = Master
+	AudioServer.set_bus_volume_db(0, db)
+
+	var config = ConfigFile.new()
+	config.set_value("audio", "master_volume", value)
+	config.save("user://settings.cfg")
 
 func _input(event):
 	if event.is_action_pressed("quit_game"):
